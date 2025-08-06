ab02d0f642a2f0d7451020c2bb6ada2d09e37388
nardis0321 <nardis0321@gmail.com>
2025-08-06 23:41:33 +0900

    feat: 스프링, 멀티모듈 초기 설정, 환경 구성
    - coupon-api, coupon-consumer, coupon-core 모듈 구성
    - 각 모듈의 기본 애플리케이션, 빌드 스크립트, 설정 파일 추가
    - docker-compose.yml 추가

---
+++ coupon-consumer/src/main/java/com/example/couponconsumer/CouponConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.example.couponconsumer;
+
+import org.example.couponcore.CouponCoreConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import(CouponCoreConfiguration.class)
+@SpringBootApplication
+public class CouponConsumerApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-core, application-consumer");
+        SpringApplication.run(CouponConsumerApplication.class, args);
+    }
+}
